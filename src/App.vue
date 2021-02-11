b8b5c6b103c3c4952c027eb59d72eab27b196e65
frankmagoba <franklinmagoba4@gmail.com>
2021-02-11 16:53:51 +0300

    setup v table

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,50 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld/>
-    </v-main>
+ <v-container>
+ <div>
+  <v-app>
+    <v-card>
+      <v-card-title>
+      </v-card-title>
+      <v-data-table
+        :headers="headers"
+        :items="songs"
+        :search="search"
+      ></v-data-table>
+    </v-card>
+   
+  </v-app>
+</div></v-container>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'App',
+  components: {},
 
-  components: {
-    HelloWorld,
-  },
-
-  data: () => ({
-    //
-  }),
+ data () {
+    return {
+      search: '',
+      headers: [
+        { text: '#', value: 'no' },
+        { text: 'Title', value: 'title' },
+        { text: 'Length', value: 'length' },
+      ],
+      songs: [
+        {
+          no: 1,
+          title: "Album track #1",
+          length: '4.09',
+        },
+        {
+         no: 1,
+          title: "Album track #2",
+          length: '4.20'
+        }
+      ]
+    }
+  }
 };
 </script>
