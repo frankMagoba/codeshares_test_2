27d532412060f1f0c096fe2799e8d2f464e5d00b
frankmagoba <franklinmagoba4@gmail.com>
2021-02-12 22:56:16 +0300

    clean up extra

--- store/store.js
+++ store/store.js
@@ -5,18 +5,15 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-    barColor: 'rgba(0, 0, 0, .8), rgba(0, 0, 0, .8)',
-    barImage: 'https://demos.creative-tim.com/material-dashboard/assets/img/sidebar-3.jpg',
-    drawer: null,
-  },
-  getters: {
-  },
+    state: {
+    },
+    getters: {
+    },
 
-  mutations: {
- 
-  },
-  actions: {
+    mutations: {
 
-  },
+    },
+    actions: {
+
+    },
 })
